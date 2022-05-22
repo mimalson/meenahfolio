bed117ff7096b7036b547a6c441fe4bc716b456d
Halal-Lab <muhaminat@gmail.com>
2022-05-22 20:22:19 +0100

    contact

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,7 +3,30 @@ import "./footer.css"
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>meenahh</a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#about">Home</a>
+        </li>
+        <li>
+          <a href="#experience">About</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </footer>
   )
 }
 
