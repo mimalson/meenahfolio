9854cd899c4f190904c2f773095c555285b479d5
Halal-Lab <muhaminat@gmail.com>
2022-05-22 11:19:09 +0100

    initializing repo

---
+++ src/components/Contact/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./contact.css"
+
+const contact = () => {
+  return (
+    <section id='contact'>
+      Conact
+    </section>
+  )
+}
+
+export default contact
---
+++ src/components/Testimonials/testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./testimonials.css"
+
+const testimonials = () => {
+  return (
+    <section id='testimonial'>
+      Testimonials
+    </section>
+  )
+}
+
+export default testimonials
