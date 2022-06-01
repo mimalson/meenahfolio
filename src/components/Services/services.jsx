37c341c8078274aa7a3a0abb2541dce439401cca
Amina <87755052+mimalson@users.noreply.github.com>
2022-06-01 12:38:56 +0100

    Update services.jsx

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -5,7 +5,7 @@ const services = () => {
   return (
     <section id='services'>
       <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h2> 04. Services</h2>
 
       <div className=" container services__container">
         
